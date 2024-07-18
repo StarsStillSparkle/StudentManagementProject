92e2c6c4b9a797a7a9d214203db9db08c336123f
Sparkle <121014662+StarsStillSparkle@users.noreply.github.com>
2024-07-18 18:11:28 +0800

    Add files via upload

---
+++ Cmd5.cs
@@ -0,0 +1,29 @@
+﻿using example.Cmd;
+using StudentData;
+using System;
+
+namespace CmdSystem
+{
+    internal class Cmd5
+    {
+        private static int ReadStudentId()
+        {
+            return Util.ReadInt("Please enter the student's ID to remove: "); // 读取学生ID
+        }
+
+        public static void HandleCmd5()
+        {
+            StudentSystem studentSystem = StudentSystem.GetInstance();
+            int id = ReadStudentId();
+            if (studentSystem.IsIdExist(id))
+            {
+                studentSystem.RemoveStudent(id);
+                Console.WriteLine("Student removed successfully"); // 移除成功
+            }
+            else
+            {
+                Console.WriteLine("This ID does not exist"); // ID不存在
+            }
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example.Cmd
+{
+    internal class Util
+    {
+        public static int ReadInt(string i)
+        {
+            int value;
+            while (true)
+            {
+                Console.WriteLine(i);
+                if (int.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+
+                Console.WriteLine("Invalid input"); // 无效输入提示
+            }
+        }
+
+    }
+}
